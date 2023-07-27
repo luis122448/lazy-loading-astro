27b995c3ab9f40f1813702184549f459a0514eba
luis122448 <luis122448@gmail.com>
2023-07-26 21:24:14 -0500

    Configuration Project for Deployment GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: ' https://luis122448.github.io',
+  base: '/lazy-loading-astro/',
   integrations: [tailwind()]
 });
